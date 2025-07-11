4afe3a31aa6e4ab5e18344b2ac81f996e0d87c31
notdeltaxd <sayankabiraj2018@gmail.com>
2025-07-11 10:14:23 +0530

    update regex

--- main/src/main/java/com/github/appujet/jiosaavn/source/JioSaavnAudioSourceManager.java
+++ main/src/main/java/com/github/appujet/jiosaavn/source/JioSaavnAudioSourceManager.java
@@ -17,7 +17,7 @@ import java.util.stream.Collectors;
 
 public class JioSaavnAudioSourceManager extends ExtendedAudioSourceManager {
     private static final Pattern JIOSAAVN_REGEX = Pattern.compile(
-            "(https?://)(www\\.)?jiosaavn\\.com/(song|album|featured|artist|s/playlist)/([a-zA-Z0-9-_]+)(/([a-zA-Z0-9-_]+))?");
+            "(?:https?://)?(?:www\\.)?(?:jiosaavn\\.com|saavn\\.com)/((?:s/)?(?:song|album|artist|featured|p/album|s/playlist))");
     private final int playlistTrackLimit;
     private final int recommendationsTrackLimit;
     public static final String SEARCH_PREFIX = "jssearch:";
@@ -48,15 +48,19 @@ public class JioSaavnAudioSourceManager extends ExtendedAudioSourceManager {
             if (!matcher.find()) {
                 return null;
             }
-            String type = matcher.group(3);
+            
+            String pathType = matcher.group(1);
+            String type = pathType.contains("song") ? "song" :
+                         pathType.contains("album") ? "album" :
+                         pathType.contains("artist") ? "artist" :
+                         pathType.contains("featured") || pathType.contains("playlist") ? "playlist" : null;
 
             switch (type) {
                 case "song":
                     return this.getTrack(audioReference.identifier);
                 case "album":
                     return this.getAlbum(audioReference.identifier);
-                case "featured":
-                case "s/playlist":
+                case "playlist":
                     return this.getPlaylist(audioReference.identifier);
                 case "artist":
                     return this.getArtist(audioReference.identifier);
